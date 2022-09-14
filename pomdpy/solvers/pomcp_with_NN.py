926093a0a39466be17ed665d7baca9e0744c1ebc
kyungohoRyu <kyungho95.ryu@gmail.com>
2022-09-14 13:40:32 +0900

    update random walk

--- pomdpy/solvers/pomcp_with_NN.py
+++ pomdpy/solvers/pomcp_with_NN.py
@@ -44,7 +44,7 @@ class POMCPWITHNN(BeliefTreeSolver):
         action_dimension = self.model.get_action_dimension()
 
         self.A2CModel = DRLModel(state_dimension, state_space, action_dimension, action_space, self.model.DRLType)
-        self.A2CSample = None
+        self.A2CSample = Sample()
 
         for N in range(POMCPWITHNN.UCB_N):
             for n in range(POMCPWITHNN.UCB_n):
